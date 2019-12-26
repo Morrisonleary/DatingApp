c384bb50d36f0b24a08a0ad89ed828e67722c0e6
Spencer Harris <SpencerH@Spencer-H-iMac.local>
2019-12-26 16:04:06 -0700

    End of section 3

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -8,5 +8,7 @@ namespace DatingApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
 
         public DbSet<value> Values { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
